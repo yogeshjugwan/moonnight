a9c8a6dc87ed2e8dc282cc373f80171aa0e12d48
yogesh-sudo <tyogeh361@gmail.com>
2022-03-27 16:44:48 +0530

    update color

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -8,8 +8,32 @@ const blog = () => {
         <title>Moon Night Group | Blogs</title>
       </Head>
       <div>
+        {/* Start Page Title Area */}
+        <div className="page-title">
+          <div className="d-table">
+            <div className="d-table-cell">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6">
+                    <div className="page-title-text">
+                      <h3>Bags &amp; Storage</h3>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6">
+                    <ul>
+                      <li><a href="index.html"><i className="fa fa-home" /> Home</a></li>
+                      <li><i className="fa fa-angle-right" /></li>
+                      <li className="active">Bags &amp; Storage</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* End Page Title Area */}
 
-        <Blog></Blog>
+        <Blog />
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Homes from './home'
 export default function Home() {
