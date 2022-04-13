e1b01032fa7f7d68d139b4100a620e20a61df8ca
yogesh-sudo <tyogeh361@gmail.com>
2022-04-13 21:37:29 +0530

    upadate form

--- pages/productsInquiry.js
+++ pages/productsInquiry.js
@@ -3,16 +3,16 @@ const productsInquiry = () => {
   return (
     <div className='moonnightgroup'>
       <iframe id="moonnightgroup"
-      ancestors="true"
-      title="moonnightgroup"
-      width="100%" height="1134"
-      frameBorder="0" marginHeight="0" marginWidth="0"
-      src="https://docs.google.com/forms/d/e/1FAIpQLSeYHvcA-K7DqH0ov3N3TRfzG0G9Wui91cB0zv94pepsgdiYSg/viewform?embedded=true">
-    </iframe>
+        ancestors="true"
+        title="moonnightgroup"
+        width="100%" height="1134"
+        frameBorder="0" marginHeight="0" marginWidth="0"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeYHvcA-K7DqH0ov3N3TRfzG0G9Wui91cB0zv94pepsgdiYSg/viewform?embedded=true">
+      </iframe>
     </div>
   )
 
-  
+
 }
 
 export default productsInquiry
