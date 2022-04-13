f571f4494e3f274d86109ca1a82cb4ed0ab7de7c
yogesh-sudo <tyogeh361@gmail.com>
2022-04-13 18:28:01 +0530

    update links

--- pages/productsInquiry.js
+++ pages/productsInquiry.js
@@ -1,18 +1,17 @@
 import React from 'react'
-
 const productsInquiry = () => {
   return (
     <div className='moonnightgroup'>
-      {/* <iframe id="moonnightgroup"
+      <iframe id="moonnightgroup"
       ancestors="true"
       title="moonnightgroup"
       width="100%" height="1134"
       frameBorder="0" marginHeight="0" marginWidth="0"
       src="https://docs.google.com/forms/d/e/1FAIpQLSeYHvcA-K7DqH0ov3N3TRfzG0G9Wui91cB0zv94pepsgdiYSg/viewform?embedded=true">
-    </iframe> */}
+    </iframe>
 
-    <div id="ff-compose"></div>
-<script async defer src="https://formfacade.com/include/106019324883741347577/form/1FAIpQLSeYHvcA-K7DqH0ov3N3TRfzG0G9Wui91cB0zv94pepsgdiYSg/clean.js?div=ff-compose"></script>
+    {/* <div id="ff-compose"></div>
+<script async defer src="../public/form"></script> */}
     </div>
   )
 
