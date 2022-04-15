3673380d2a722d1674888f7c7fb0874fd88c415e
yogesh-sudo <tyogeh361@gmail.com>
2022-04-15 18:04:57 +0530

    uadate

--- pages/productsInquiry.js
+++ pages/productsInquiry.js
@@ -7,7 +7,7 @@ const productsInquiry = () => {
         className={styles.moonnightgroup}
         ancestors="true"
         title="moonnightgroup"
-        allowfullscreen
+        allowFullScreen
         scrolling="none"
         frameBorder="0" marginHeight="0" marginWidth="0"
         src="https://docs.google.com/forms/d/e/1FAIpQLSeYHvcA-K7DqH0ov3N3TRfzG0G9Wui91cB0zv94pepsgdiYSg/viewform?embedded=true">
