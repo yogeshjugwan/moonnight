4f98dd8fb672fd02b7dc6c776d6a61512d27a917
yogesh-sudo <tyogeh361@gmail.com>
2022-04-15 18:04:05 +0530

    uadate

--- pages/productsInquiry.js
+++ pages/productsInquiry.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import styles from '../styles/Home.module.css'
 const productsInquiry = () => {
   return (
-    <div className='moonnightgroup '>
+    <div>
       <iframe id="moonnightgroup"
-      className='overflow-hidden'
+        className={styles.moonnightgroup}
         ancestors="true"
         title="moonnightgroup"
         allowfullscreen
         scrolling="none"
-        width="100%" height="434"
         frameBorder="0" marginHeight="0" marginWidth="0"
         src="https://docs.google.com/forms/d/e/1FAIpQLSeYHvcA-K7DqH0ov3N3TRfzG0G9Wui91cB0zv94pepsgdiYSg/viewform?embedded=true">
       </iframe>
